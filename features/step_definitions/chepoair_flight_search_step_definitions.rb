c3da564fb13dbf85331be11c4343fdccfadfb464
Saras <vadlasaraswathi@gmail.com>
2021-10-31 22:32:14 -0400

    Updated the class name based on Ravi's comment

--- features/step_definitions/chepoair_flight_search_step_definitions.rb
+++ features/step_definitions/chepoair_flight_search_step_definitions.rb
@@ -1,40 +1,40 @@
 Given(/^user is on the Cheapoair homepage$/) do
-  visit CheapoairHomePageClass
-  on(CheapoairHomePageClass).maximizing_window
+  visit CheapoairHomePage
+  on(CheapoairHomePage).maximizing_window
 end
 
 When(/^user selects the Round Trip$/) do
-  on(CheapoairHomePageClass).select_round_trip
-  on(CheapoairHomePageClass).round_trip_tab_element.click if on(CheapoairHomePageClass).round_trip_tab_element.present?
-  on(CheapoairHomePageClass).round_trip_tab.click if on(CheapoairHomePageClass).round_trip_tab_element.present?
+  on(CheapoairHomePage).select_round_trip
+  on(CheapoairHomePage).round_trip_tab_element.click if on(CheapoairHomePage).round_trip_tab_element.present?
+  on(CheapoairHomePage).round_trip_tab.click if on(CheapoairHomePage).round_trip_tab_element.present?
 end
 
 And(/^user search the (.*) city and selects (.*) airport for departure$/) do |dep_city, dep_airport_code|
-  on(CheapoairHomePageClass).search_dep_airport dep_city, dep_airport_code
+  on(CheapoairHomePage).search_dep_airport dep_city, dep_airport_code
 end
 
 And(/^user search for (.*) city and selects (.*) airport for arrival$/) do |arr_city, arr_airport_code|
-  on(CheapoairHomePageClass).search_arr_airport arr_city, arr_airport_code
+  on(CheapoairHomePage).search_arr_airport arr_city, arr_airport_code
 end
 
 And(/^user selects future dates for departure (.*) days from today$/) do |no_of_days|
-  on(CheapoairHomePageClass).choose_dep_date no_of_days
+  on(CheapoairHomePage).choose_dep_date no_of_days
 end
 
 And(/^user selects future dates for arrival (.*) days from today$/) do |no_of_days|
-  on(CheapoairHomePageClass).choose_arr_date no_of_days
+  on(CheapoairHomePage).choose_arr_date no_of_days
 end
 
 And(/^user searches for available for flights$/) do
-  on(CheapoairHomePageClass).search_flights
+  on(CheapoairHomePage).search_flights
 end
 
  Then(/^verify user should see the available flights results$/) do
-  on(CheapoairHomePageClass).verify_flight_results
+  on(CheapoairHomePage).verify_flight_results
 end
 
 When(/^user searches for the available flights for the future dates$/) do
-  on(CheapoairHomePageClass) do |page|
+  on(CheapoairHomePage) do |page|
     page.select_round_trip
     page.round_trip_tab_element.click if page.round_trip_tab_element.present?
     page.round_trip_tab.click if page.round_trip_tab_element.present?
